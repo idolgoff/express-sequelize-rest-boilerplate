1a8cda80102c6f811644df0ad9f914ccf70d7f08
idolgoff <dolgov.ivan@gmail.com>
2017-12-05 13:33:56 +0500

    + webpack + typescript + tsconfig + eslint

---
+++ webpack/common.js
@@ -0,0 +1,44 @@
+const webpack = require("webpack");
+const path = require("path");
+const Visualizer = require('webpack-visualizer-plugin');
+
+/*global __dirname*/
+/*eslint no-undef: "error"*/
+const PATHS = {
+    source: path.join(__dirname, '../src'),
+    build: path.join(__dirname, '../build')
+};
+
+module.exports = {
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".jsx"]
+    },
+    entry: PATHS.source + '/index.ts' ,
+    output: {
+        path: PATHS.build,
+        filename: "[name].js",
+    },
+    module: {
+        loaders: [
+            // load ts/tsx with ts-loader
+            {
+                test: /\.tsx?$/,
+                loader: "ts-loader",
+                options: {
+                    transpileOnly: true,
+                    compilerOptions: {
+                        // use target=es5 for old browsers
+                        target: "es5",
+                        // use module=es6 for tree-shaking and stuff
+                        module: "ES6"
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new Visualizer(),
+        new webpack.NoEmitOnErrorsPlugin()
+    ],
+    devtool: "source-map"
+};
